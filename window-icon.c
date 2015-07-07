050dd22aec9a0d819a028551cd4f5134c4f2c8dc
SametSisartenep <rodrigosloop@gmail.com>
2015-07-07 02:10:44 +0200

    GTK+ Window Icon example uploaded.

---
+++ window-icon.c
@@ -0,0 +1,34 @@
+#include <gtk-2.0/gtk/gtk.h>
+
+GdkPixbuf* create_pixbuf ( const gchar *filename ) {
+  GdkPixbuf *pixbuf;
+  GError *error = NULL;
+
+  pixbuf = gdk_pixbuf_new_from_file(filename, &error);
+
+  if (!pixbuf) {
+    fprintf(stderr, "%s\n", error->message);
+    g_error_free(error);
+  }
+
+  return pixbuf;
+}
+
+int main ( int argc, char *argv[] ) {
+  GtkWidget *window;
+
+  gtk_init( &argc, &argv );
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Window Icon");
+  gtk_window_set_default_size(GTK_WINDOW(window), 230, 150);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("icon.png"));
+  gtk_widget_show(window);
+
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_main();
+
+  return 0;
+}
